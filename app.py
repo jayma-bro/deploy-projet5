c380240cfbf0a94469ab9478d6b1c44470fa55f6
skamos <skamosbllurp@gmail.com>
2022-04-14 00:58:44 +0200

    correction en local

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ model = Model()
 
 @app.route("/")
 def index():
-	return render_template("index.html", context='rien')
+	return render_template("index.html", context= 'rien')
 
 @app.route("/submit", methods=['POST', 'GET'])
 def greeter():
-	return render_template("index.html", context=model.predict(request.form['title'], request.form['body']))  # type: ignore
+	return render_template("index.html", context = model.predict(request.form['title'], request.form['body']))  # type: ignore
 
 if __name__ == "__main__":
     app.run(debug=True)
